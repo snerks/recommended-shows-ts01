4bc6e21c47e1ca1155a50dc4a988c3485e7d9fa5
snerks <martin.github@outlook.com>
2018-09-17 18:38:43 +0100

    Use Fetch

--- src/App.tsx
+++ src/App.tsx
@@ -5,41 +5,77 @@ import "./App.css";
 // import logo from './logo.svg';
 import { ShowDate } from "./show-dates";
 
-class App extends React.Component {
-  showDates: ShowDate[] = [
-    {
-      date: new Date("2018-09-17T20:00:00.000Z"),
-      shows: [
-        {
-          artists: [
-            {
-              name: "Chuck Prophet and Stephanie Finch"
-            }
-          ],
-          venue: "Hen and Chicken"
-        }
-      ]
-    },
-    {
-      date: new Date("2018-09-21T20:00:00.000Z"),
-      shows: [
-        {
-          artists: [
-            {
-              name: "Nightingales"
-            },
-            {
-              name: "Stewart Lee"
-            }
-          ],
-          venue: "Exchange"
-        }
-      ]
-    }
-  ];
+interface AppState {
+  showDates: ShowDate[];
+
+  error?: any;
+}
+
+class App extends React.Component<{}, AppState> {
+  constructor(props: {}, state: AppState = { showDates: [] }) {
+    super(props, state);
+
+    this.state = state;
+  }
+
+  // showDates: ShowDate[] = [
+  //   {
+  //     date: new Date("2018-09-17T20:00:00.000Z"),
+  //     shows: [
+  //       {
+  //         artists: [
+  //           {
+  //             name: "Chuck Prophet and Stephanie Finch"
+  //           }
+  //         ],
+  //         venue: "Hen and Chicken"
+  //       }
+  //     ]
+  //   },
+  //   {
+  //     date: new Date("2018-09-21T20:00:00.000Z"),
+  //     shows: [
+  //       {
+  //         artists: [
+  //           {
+  //             name: "Nightingales"
+  //           },
+  //           {
+  //             name: "Stewart Lee"
+  //           }
+  //         ],
+  //         venue: "Exchange"
+  //       }
+  //     ]
+  //   }
+  // ];
 
   componentDidMount() {
-    console.log("TODO: fetch data");
+    // console.log("Fetch data : start");
+    fetch("recommended-shows.json")
+      .then(res => {
+        console.log("Success", res);
+        return res.json();
+      })
+      .then(
+        result => {
+          console.log("SetState with: ", result);
+          const nextState: AppState = {
+            showDates: result as ShowDate[],
+            error: undefined
+          };
+
+          this.setState(nextState);
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          this.setState({
+            error
+          });
+        }
+      );
   }
 
   public render() {
@@ -55,13 +91,17 @@ class App extends React.Component {
     // </div>
     // );
 
-    if (this.showDates.length === 0) {
+    const { showDates, error } = this.state;
+
+    if (error) {
+      return <div>Error: {JSON.stringify(error)}</div>;
+    }
+
+    if (!showDates || showDates.length === 0) {
       return <div>Loading Shows...</div>;
     }
 
-    // moment().format('MMMM Do YYYY, h:mm:ss a')
-
-    const tableRows = this.showDates.map(showDate =>
+    const tableRows = showDates.map(showDate =>
       showDate.shows.map((show, showIndex) => (
         <tr key={showIndex}>
           <td>{moment(showDate.date).format("dddd")}</td>
